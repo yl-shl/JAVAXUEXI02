4f45038488fe6c79fc48d4a61fe6874c7019d168
yl-shl <74883973+yl-shl@users.noreply.github.com>
2020-11-30 16:06:30 +0800

    Create 113001.继承子类.APP2.java

---
+++ 113001.继承子类.APP2.java
@@ -0,0 +1,9 @@
+package java113002;
+
+public class R {
+	static String boy_filePath="img/boy.gif";
+	static String girl_filePath="img/girl.gif";
+	static int [][]array2d={{1,2},
+		{3,4},
+		{5,6}};                                      
+}
